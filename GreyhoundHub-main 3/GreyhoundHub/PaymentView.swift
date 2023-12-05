faa3a596f02be021a25a261dd188931ead35143c
Alex Kristian <70616781+Alex-Kristian@users.noreply.github.com>
2023-12-05 02:05:37 -0500

    Update PaymentView.swift

--- GreyhoundHub-main 3/GreyhoundHub/PaymentView.swift
+++ GreyhoundHub-main 3/GreyhoundHub/PaymentView.swift
@@ -2,7 +2,7 @@
 //  PaymentView.swift
 //  GreyhoundHub
 //
-//  Created by Alex Kristian on 12/4/23.
+//  Created by Alex Kristian on 12/5/23.
 //
 
 import Foundation
@@ -11,24 +11,35 @@ import SwiftUI
 struct PaymentView: View {
     @State private var isPaymentSuccessful = false
     @Binding var isShowingPaymentView: Bool
+    @Binding var isShowingChoices: Bool
    
     var body: some View {
-        VStack {
-            if isPaymentSuccessful {
-                Text("Payment Successful!")
-            } else {
-                Button("Pay with Apple Pay") {
-                    // Simulate payment processing
-                    simulatePayment()
+        ZStack {
+            LinearGradient(
+                colors: [.mint, .green],
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing
+            )
+            
+            .ignoresSafeArea()
+            VStack {
+                if isPaymentSuccessful {
+                    Text("Payment Successful!")
+                } else {
+                    Button("Pay with Apple Pay") {
+                        // Simulate payment processing
+                        simulatePayment()
+                        isShowingChoices = false
+                        isShowingPaymentView = false
+                    }
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
                 }
-                .padding()
-                .background(Color.blue)
-                .foregroundColor(.white)
-                .cornerRadius(8)
             }
         }
     }
-   
     func simulatePayment() {
        
         DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
