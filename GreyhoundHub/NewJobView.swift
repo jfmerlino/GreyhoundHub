5c538e3214321f4c9bff27a4df0717300eb0909b
rjgehan <rjgehan@loyola.edu>
2023-12-04 22:27:16 -0500

    new endpoint

--- GreyhoundHub/NewJobView.swift
+++ GreyhoundHub/NewJobView.swift
@@ -1,5 +1,6 @@
 import SwiftUI
 
+// UserData struct remains the same
 struct UserData: Identifiable {
     let id = UUID()
     let ghName: String
@@ -10,10 +11,27 @@ struct UserData: Identifiable {
     let currOrder: String?
 }
 
+// UserDetailsView now takes an array of String
+struct UserDetailsView: View {
+    var userDetails: [String]
+    var body: some View {
+        VStack {
+            Text("Order Details")
+                .font(.title)
+            List(userDetails, id: \.self) { detail in
+                Text(detail)
+            }
+        }
+    }
+}
+
+// NewJobView is updated to split the currOrder string
 struct NewJobView: View {
     @State private var apiService = APIService()
     @Binding var showingSheet: Bool
     @State var users = [UserData]()
+    @State private var userDetails: [String] = []
+    @State private var showUserDetails = false
 
     var body: some View {
         ZStack {
@@ -34,20 +52,37 @@ struct NewJobView: View {
                     }
                     .padding()
                 }
-                Spacer()
 
-                // Displaying each UserData separately
-                ForEach(users) { user in
-                    DebugTextView(userDescription: "\(user.username): \(user.currOrder)")
-                        .foregroundColor(.white)
+                ScrollView {
+                    ForEach(users.filter { $0.currOrder != nil }, id: \.id) { user in
+                        Button(action: {
+                            if let currOrder = user.currOrder {
+                                self.userDetails = currOrder.components(separatedBy: ", ").filter { !$0.isEmpty }
+                            }
+                            self.showUserDetails = true
+                        }) {
+                            Text(user.username) // Display the user's username
+                                .foregroundColor(.white)
+                                .padding()
+                                .background(Color.black.opacity(0.5))
+                                .clipShape(RoundedRectangle(cornerRadius: 10))
+                        }
+                        .padding(.horizontal)
+                    }
                 }
             }
+        }
+        .sheet(isPresented: $showUserDetails, onDismiss: {
+            //self.userDetails = []
+            MapView(dropOffPoint: userDetails, dropOffLat: 0.0, dropOffLong: 0.0)
+        }) {
+            UserDetailsView(userDetails: self.userDetails)
         }
         .onAppear {
             apiService.getAllUsers { result in
                 switch result {
                 case .success(let fetchedUsers):
-                    users = fetchedUsers.map { userDataDict in
+                    self.users = fetchedUsers.map { userDataDict in
                         UserData(
                             ghName: userDataDict["ghName"] as? String ?? "",
                             dropoff: userDataDict["dropoff"] as? String ?? "",
@@ -58,18 +93,9 @@ struct NewJobView: View {
                         )
                     }
                 case .failure(let error):
-                    // Handle the error
                     print("Error fetching users: \(error)")
                 }
             }
         }
-    }
-}
-
-struct DebugTextView: View {
-    var userDescription: String
-
-    var body: some View {
-        Text(userDescription)
     }
 }
