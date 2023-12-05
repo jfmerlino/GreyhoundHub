f4dc875f8cfc5f1cdf07ff161a6275ed74e21e9c
alexkristian <ajkristian@loyola.edu>
2023-12-04 20:30:09 -0500

    Commit

--- GreyhoundHub/GreenGreyView.swift
+++ GreyhoundHub/GreenGreyView.swift
@@ -9,7 +9,8 @@ struct CreateGreenGreyView: View {
     @State var locationDropoff = ""
     @State var extra = ""
     @State var locationDropoffIndex = 0
-
+    @State var orderStatus = "New"
+    @State var name = "Green and Grey"
     
     @State private var storedInputs: [String] = [] // Storing inputs in a list
     @State private var apiService = APIService() // Instance of APIService
@@ -133,19 +134,21 @@ struct CreateGreenGreyView: View {
 
     
     func storeInputs() {
-            storedInputs = [grubhubNumber, grubhubName, beingPickedUp, locations[locationDropoffIndex], extra]
+            storedInputs = [orderStatus, name, grubhubNumber, grubhubName, beingPickedUp, locations[locationDropoffIndex], extra]
             UserDefaults.standard.set(storedInputs, forKey: "StoredInputsKey")
         }
     
     func loadInputs() {
         if let inputs = UserDefaults.standard.stringArray(forKey: "StoredInputsKey") {
             storedInputs = inputs
-            if storedInputs.count == 5 { // Assuming 5 inputs
-                grubhubNumber = storedInputs[0]
-                grubhubName = storedInputs[1]
-                beingPickedUp = storedInputs[2]
-                locationDropoff = storedInputs[3]
-                extra = storedInputs[4]
+            if storedInputs.count == 6 { // Assuming 5 inputs
+                orderStatus = storedInputs[0]
+                name = storedInputs[1]
+                grubhubNumber = storedInputs[2]
+                grubhubName = storedInputs[3]
+                beingPickedUp = storedInputs[4]
+                locationDropoff = storedInputs[5]
+                extra = storedInputs[6]
             }
         }
     }
