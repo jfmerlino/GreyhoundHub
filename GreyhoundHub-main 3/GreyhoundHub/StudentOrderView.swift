5529be3ebf9c468fda919711d5a22d40ece86da9
Alex Kristian <70616781+Alex-Kristian@users.noreply.github.com>
2023-12-05 02:06:15 -0500

    Update StudentOrderView.swift

--- GreyhoundHub-main 3/GreyhoundHub/StudentOrderView.swift
+++ GreyhoundHub-main 3/GreyhoundHub/StudentOrderView.swift
@@ -34,20 +34,25 @@ struct StudentOrderView: View {
             .ignoresSafeArea()
             if(hasOrder(userDetails: userDetails)){
                 VStack {
+                    titleView
                     
                     if (orderStatus == "New") {
                         Text("Order #" + orderNum)
                             .font(.system(size: 30))
                             .fontWeight(.bold)
+                            .padding()
                         Text("Waiting for a worker to pick up your order...")
-                            .font(.system(size: 30))
+                            .font(.system(size: 20))
                             .fontWeight(.bold)
+                            .padding()
                         Text("You Ordered: " + whatOrdered)
-                            .font(.system(size: 30))
+                            .font(.system(size: 20))
                             .fontWeight(.bold)
-                        Text("\(fromLoc) -> \(orderNum)")
-                            .font(.system(size: 30))
+                            .padding()
+                        Text("\(fromLoc) -> \(toLoc)")
+                            .font(.system(size: 20))
                             .fontWeight(.bold)
+                            .padding()
                         
                         
                         Button("Refresh"){
@@ -91,52 +96,104 @@ struct StudentOrderView: View {
                         
                     }
                     else{
-                        Text("No Order To View")
-                        Button("Refresh"){
-                            userDetails = []
-                            apiService.getAllUsers { result in
-                                switch result {
-                                case .success(let fetchedUsers):
-                                    self.users = fetchedUsers.map { userDataDict in
-                                        UserData(
-                                            ghName: userDataDict["ghName"] as? String ?? "",
-                                            dropoff: userDataDict["dropoff"] as? String ?? "",
-                                            isWorker: userDataDict["isWorker"] as? Bool ?? false,
-                                            username: userDataDict["username"] as? String ?? "",
-                                            password: userDataDict["password"] as? String ?? "",
-                                            currOrder: userDataDict["currOrder"] as? String
-                                        )
-                                    }
-                                    
-                                    for person in self.users.filter({ $0.currOrder != nil }) {
-                                        if person.username == self.user, let currOrder = person.currOrder {
-                                            self.userDetails = currOrder.components(separatedBy: ", ").filter { !$0.isEmpty }
-                                            orderStatus = userDetails[0]
-                                            fromLoc = userDetails[1]
-                                            //order num
-                                            orderNum = userDetails[2]
-                                            //grub nam
-                                            grubName = userDetails[3]
-                                            //what ordered
-                                            whatOrdered = userDetails[4]
-                                            //location to
-                                            toLoc = userDetails[5]
-                                            //notes
+                        VStack{
+                            titleView
+                            Text("No Order To View")
+                                .font(.system(size: 30))
+                                .fontWeight(.bold)
+                            Button("Refresh"){
+                                userDetails = []
+                                apiService.getAllUsers { result in
+                                    switch result {
+                                    case .success(let fetchedUsers):
+                                        self.users = fetchedUsers.map { userDataDict in
+                                            UserData(
+                                                ghName: userDataDict["ghName"] as? String ?? "",
+                                                dropoff: userDataDict["dropoff"] as? String ?? "",
+                                                isWorker: userDataDict["isWorker"] as? Bool ?? false,
+                                                username: userDataDict["username"] as? String ?? "",
+                                                password: userDataDict["password"] as? String ?? "",
+                                                currOrder: userDataDict["currOrder"] as? String
+                                            )
                                         }
+                                        
+                                        for person in self.users.filter({ $0.currOrder != nil }) {
+                                            if person.username == self.user, let currOrder = person.currOrder {
+                                                self.userDetails = currOrder.components(separatedBy: ", ").filter { !$0.isEmpty }
+                                                orderStatus = userDetails[0]
+                                                fromLoc = userDetails[1]
+                                                //order num
+                                                orderNum = userDetails[2]
+                                                //grub nam
+                                                grubName = userDetails[3]
+                                                //what ordered
+                                                whatOrdered = userDetails[4]
+                                                //location to
+                                                toLoc = userDetails[5]
+                                                //notes
+                                            }
+                                        }
+                                        
+                                    case .failure(let error):
+                                        print("Error fetching users: \(error)")
                                     }
-                                    
-                                case .failure(let error):
-                                    print("Error fetching users: \(error)")
                                 }
                             }
                         }
+                        
                     }
-                    
                 }
             }
             else{
-                Text("No Order To View")
-                Button("Refresh"){
+                VStack{
+                    titleView
+                    Text("No Order To View")
+                        .font(.system(size: 30))
+                        .fontWeight(.bold)
+                    Button("Refresh"){
+                        userDetails = []
+                        apiService.getAllUsers { result in
+                            switch result {
+                            case .success(let fetchedUsers):
+                                self.users = fetchedUsers.map { userDataDict in
+                                    UserData(
+                                        ghName: userDataDict["ghName"] as? String ?? "",
+                                        dropoff: userDataDict["dropoff"] as? String ?? "",
+                                        isWorker: userDataDict["isWorker"] as? Bool ?? false,
+                                        username: userDataDict["username"] as? String ?? "",
+                                        password: userDataDict["password"] as? String ?? "",
+                                        currOrder: userDataDict["currOrder"] as? String
+                                    )
+                                }
+                                
+                                for person in self.users.filter({ $0.currOrder != nil }) {
+                                    if person.username == self.user, let currOrder = person.currOrder {
+                                        self.userDetails = currOrder.components(separatedBy: ", ").filter { !$0.isEmpty }
+                                        orderStatus = userDetails[0]
+                                        fromLoc = userDetails[1]
+                                        //order num
+                                        orderNum = userDetails[2]
+                                        //grub nam
+                                        grubName = userDetails[3]
+                                        //what ordered
+                                        whatOrdered = userDetails[4]
+                                        //location to
+                                        toLoc = userDetails[5]
+                                        //notes
+                                    }
+                                }
+                                
+                            case .failure(let error):
+                                print("Error fetching users: \(error)")
+                            }
+                        }
+                    }
+                }
+                
+            }
+        }
+                    
+                .onAppear(perform: {
                     userDetails = []
                     apiService.getAllUsers { result in
                         switch result {
@@ -173,47 +230,6 @@ struct StudentOrderView: View {
                             print("Error fetching users: \(error)")
                         }
                     }
-                }
-            }
-            
-        }
-                .onAppear(perform: {
-                    userDetails = []
-                    apiService.getAllUsers { result in
-                        switch result {
-                        case .success(let fetchedUsers):
-                            self.users = fetchedUsers.map { userDataDict in
-                                UserData(
-                                    ghName: userDataDict["ghName"] as? String ?? "",
-                                    dropoff: userDataDict["dropoff"] as? String ?? "",
-                                    isWorker: userDataDict["isWorker"] as? Bool ?? false,
-                                    username: userDataDict["username"] as? String ?? "",
-                                    password: userDataDict["password"] as? String ?? "",
-                                    currOrder: userDataDict["currOrder"] as? String
-                                )
-                            }
-                            
-                            for person in self.users.filter({ $0.currOrder != nil }) {
-                                if person.username == self.user, let currOrder = person.currOrder {
-                                    self.userDetails = currOrder.components(separatedBy: ", ").filter { !$0.isEmpty }
-                                    orderStatus = userDetails[0]
-                                    fromLoc = userDetails[1]
-                                    //order num
-                                    orderNum = userDetails[2]
-                                    //grub nam
-                                    grubName = userDetails[3]
-                                    //what ordered
-                                    whatOrdered = userDetails[4]
-                                    //location to
-                                    //toLoc = userDetails[5]
-                                    //notes
-                                }
-                            }
-                            
-                        case .failure(let error):
-                            print("Error fetching users: \(error)")
-                        }
-                    }
                 })
             
     }
@@ -234,5 +250,3 @@ var titleView: some View {
         .fontWeight(.bold)
         .foregroundColor(.white)
 }
-
-
